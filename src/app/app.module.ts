09f6f83b5abcb883358aec1a2d6b3f70685ea441
Student 235748 <235748@student.pwr.edu.pl>
2019-11-14 23:36:49 +0100

    Init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { ApartmentModule } from './pages/apartment/apartment.module';
+import { UserModule } from './pages/user/user.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/shared/layout/navbar/navbar.component';
+import { ContainerComponent } from './pages/shared/layout/container/container.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ApartmentModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/apartment/apartment.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ApartmentRoutingModule } from './apartment-routing.module';
+import { ApartmentListComponent } from './apartment-list/apartment-list.component';
+import { ApartmentEditComponent } from './apartment-edit/apartment-edit.component';
+import { ApartmentCreateComponent } from './apartment-create/apartment-create.component';
+import { ApartmentDetailsComponent } from './apartment-details/apartment-details.component';
+
+
+@NgModule({
+  declarations: [ApartmentListComponent, ApartmentEditComponent, ApartmentCreateComponent, ApartmentDetailsComponent],
+  imports: [
+    CommonModule,
+    ApartmentRoutingModule
+  ]
+})
+export class ApartmentModule { }

6b10d8c27227af8dc634cd33ffd74a29d89f0d32
Student 235748 <235748@student.pwr.edu.pl>
2019-11-15 21:28:14 +0100

    fixed routings, fixed navbar

--- src/app/pages/user/user-routing.module.ts
+++ src/app/pages/user/user-routing.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
-import { UserCreateComponent } from './user-create/user-create.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
-  { path: 'user/{:id}/edit', component: UserEditComponent },
-  { path: 'user/add', component: UserCreateComponent },
-  { path: 'user/{:id}', component: UserDetailsComponent }
+  { path: 'user/:id/edit', component: UserEditComponent },
+  { path: 'user/:id', component: UserDetailsComponent }
 ];
 
 @NgModule({
--- src/app/pages/user/user.module.ts
+++ src/app/pages/user/user.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
-import { UserCreateComponent } from './user-create/user-create.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
-  declarations: [UserListComponent, UserCreateComponent, UserDetailsComponent, UserEditComponent],
+  declarations: [UserListComponent, UserDetailsComponent, UserEditComponent],
   imports: [
     CommonModule,
     UserRoutingModule
